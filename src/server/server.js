5c8005dc0b264c8265638fd2d4bed5a0793193be
Facundo <santillanfacundo43@gmail.com>
2024-07-10 12:37:58 -0300

    Agregando conexion a DB

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import '../dbConnection/dbConnection.js';
+import cors from 'cors';
+export const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: 'https://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+app.listen(PORT, () => {
+  console.log(`Estamos escuchando el puerto ${PORT}`);
+});
