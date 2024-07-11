346c0d2a83e2a5970a1c4fa4901802ea123f35ac
Facundo <santillanfacundo43@gmail.com>
2024-07-11 13:04:03 -0300

    Agregando controlador de usuario  y validaciones

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import '../dbConnection/dbConnection.js';
 import cors from 'cors';
+import userRoutes from "../routes/user.routes.js";
+
+
+export const ADMIN_KEY = process.env.ADMIN_KEY;
+export const USER_KEY = process.env.USER_KEY;
 export const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -10,12 +15,15 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: 'https://localhost:5173',
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+app.use("/user", userRoutes);
+
 app.listen(PORT, () => {
   console.log(`Estamos escuchando el puerto ${PORT}`);
 });
