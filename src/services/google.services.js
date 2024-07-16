6d1ab691262402c8f659c89480f53c2b4d61e07b
Facundo <santillanfacundo43@gmail.com>
2024-07-16 10:25:07 -0300

    Version 2 con dotenv google

--- src/services/google.services.js
+++ src/services/google.services.js
@@ -1,5 +1,6 @@
 import { OAuth2Client } from 'google-auth-library';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export async function verifyGoogleToken(token) {
