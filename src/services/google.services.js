3e7070f330045b5cebbc9eb67ad287180cfe74a9
Facundo <santillanfacundo43@gmail.com>
2024-07-16 10:57:53 -0300

    Version 4 cambiando user controll y google service

--- src/services/google.services.js
+++ src/services/google.services.js
@@ -3,10 +3,15 @@ import { OAuth2Client } from 'google-auth-library';
 const client = new OAuth2Client('793959150532-n8h7ji8lere1c2ok9q262nul33tmpftf.apps.googleusercontent.com');
 
 export async function verifyGoogleToken(token) {
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: '793959150532-n8h7ji8lere1c2ok9q262nul33tmpftf.apps.googleusercontent.com',
-  });
-  const payload = ticket.getPayload();
-  return payload;
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: '793959150532-n8h7ji8lere1c2ok9q262nul33tmpftf.apps.googleusercontent.com',
+    });
+    const payload = ticket.getPayload();
+    return payload;
+  } catch (error) {
+    console.error('Error verifying Google token:', error);
+    return null;
+  }
 }
